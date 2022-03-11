cb52ff89144031689655192f95b1e3f025ef51f5
Seong Young Jeong <tjdduddl10@gmail.com>
2022-03-11 15:50:51 +0900

    ♻️ [#16] Refactor room event in socket
    room 소켓이벤트에서 처리하던 채팅 roomManager로 이동
    chat io 서버 기반으로 수정

--- src/server/socket/chat.ts
+++ src/server/socket/chat.ts
@@ -1,9 +1,11 @@
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
 import { MessageType, TChat } from 'shared/types';
 
 let chatId = 0;
 
-const makeSystemMessage = (message: string): TChat => ({
+const emitChat = (io: Server, roomId: number, chat: TChat) => io.to(roomId.toString()).emit('chat', chat);
+
+const makeAlert = (message: string): TChat => ({
   type: MessageType.System,
   id: chatId++,
   message,
@@ -18,35 +20,31 @@ const makeChat = (name: string, imgUrl: string, message: string): TChat => ({
 });
 
 const chat = (io: Server, roomId: number, name: string, imgUrl: string, message: string) =>
-  io.to(roomId.toString()).emit('chat', makeChat(name, imgUrl, message));
+  emitChat(io, roomId, makeChat(name, imgUrl, message));
 
-const enter = (socket: Socket, roomId: number, name: string) => {
-  socket.broadcast.to(roomId.toString()).emit('chat', makeSystemMessage(`${name}님이 참여했습니다.`));
-};
+const enter = (io: Server, roomId: number, name: string) =>
+  emitChat(io, roomId, makeAlert(`${name}님이 참여했습니다.`));
 
-const leave = (socket: Socket, roomId: number, name: string) =>
-  socket.broadcast.to(roomId.toString()).emit('chat', makeSystemMessage(`${name}님이 퇴장했습니다.`));
+const leave = (io: Server, roomId: number, name: string) =>
+  emitChat(io, roomId, makeAlert(`${name}님이 퇴장했습니다.`));
 
-const start = (io: Server, roomId: number) =>
-  io.to(roomId.toString()).emit('chat', makeSystemMessage('게임이 시작되었습니다.'));
+const start = (io: Server, roomId: number) => emitChat(io, roomId, makeAlert('게임이 시작되었습니다.'));
 
 const win = (io: Server, roomId: number, name: string) =>
-  io.to(roomId.toString()).emit('chat', makeSystemMessage(`${name}님이 정답을 맞혔습니다.`));
+  emitChat(io, roomId, makeAlert(`${name}님이 정답을 맞혔습니다.`));
 
 const timeout = (io: Server, roomId: number, answer: string) => {
-  io.to(roomId.toString()).emit('chat', makeSystemMessage('제한시간이 끝났습니다.'));
-  io.to(roomId.toString()).emit('chat', makeSystemMessage(`정답은 ${answer}입니다.`));
+  emitChat(io, roomId, makeAlert('제한시간이 끝났습니다.'));
+  emitChat(io, roomId, makeAlert(`정답은 ${answer}입니다.`));
 };
 
 const painterOut = (io: Server, roomId: number, answer: string) => {
-  io.to(roomId.toString()).emit('chat', makeSystemMessage('출제자가 나가 세트가 종료되었습니다.'));
-  io.to(roomId.toString()).emit('chat', makeSystemMessage(`정답은 ${answer}입니다.`));
+  emitChat(io, roomId, makeAlert('출제자가 나가 세트가 종료되었습니다.'));
+  emitChat(io, roomId, makeAlert(`정답은 ${answer}입니다.`));
 };
 
-const stop = (io: Server, roomId: number) =>
-  io.to(roomId.toString()).emit('chat', makeSystemMessage('인원이 부족해 게임이 중단되었습니다.'));
+const stop = (io: Server, roomId: number) => emitChat(io, roomId, makeAlert('인원이 부족해 게임이 중단되었습니다.'));
 
-const end = (io: Server, roomId: number) =>
-  io.to(roomId.toString()).emit('chat', makeSystemMessage('게임이 종료되었습니다.'));
+const end = (io: Server, roomId: number) => emitChat(io, roomId, makeAlert('게임이 종료되었습니다.'));
 
 export default { chat, enter, leave, start, win, timeout, painterOut, stop, end };
