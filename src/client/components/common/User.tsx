8772363f70e2d425fb8e36e8a015415f584082f1
Seong Young Jeong <tjdduddl10@gmail.com>
2022-04-01 17:21:41 +0900

    💄 [#32] Add skeleton component in User
    User 컴포넌트에 로딩중 표시될 스켈레톤 컴포넌트 추가

--- src/client/components/common/User.tsx
+++ src/client/components/common/User.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useEffect, useRef, useState } from 'react';
 import { PROFILE_IMAGE_SIZE } from 'shared/constant';
 
 const UserWrapper = styled.div`
@@ -14,16 +15,33 @@ const UserImg = styled.img`
   border-radius: 50%;
   margin-right: 4px;
 `;
+const SkeletonImg = styled.div`
+  position: absolute;
+  width: ${PROFILE_IMAGE_SIZE}px;
+  height: ${PROFILE_IMAGE_SIZE}px;
+  border-radius: 50%;
+  background-color: #dcd8d2;
+`;
 const UserName = styled.div`
   margin-right: 4px;
   color: #000;
 `;
 
-const User = ({ name, imgUrl }: { name: string; imgUrl: string }) => (
-  <UserWrapper>
-    <UserImg src={imgUrl} alt="user profile image" />
-    <UserName>{name}</UserName>
-  </UserWrapper>
-);
+const User = ({ name, imgUrl }: { name: string; imgUrl: string }) => {
+  const [isLoading, setLoading] = useState(false);
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    setLoading(imgRef.current.complete);
+  }, [imgUrl]);
+
+  return (
+    <UserWrapper>
+      <UserImg src={imgUrl} onLoad={() => setLoading(true)} ref={imgRef} alt="user profile image" />
+      {!isLoading && <SkeletonImg />}
+      <UserName>{name}</UserName>
+    </UserWrapper>
+  );
+};
 
 export default User;
