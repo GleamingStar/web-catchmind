0cfb0437f0e783f24ff202a9673c5562d7f30980
Seong Young Jeong <tjdduddl10@gmail.com>
2022-04-06 13:56:29 +0900

    💫 [#32] Add animation to skeleton of User
    User컴포넌트의 스켈레톤에 애니메이션 적용

--- src/client/components/common/User.tsx
+++ src/client/components/common/User.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { useEffect, useRef, useState } from 'react';
 import { PROFILE_IMAGE_SIZE } from 'shared/constant';
 
@@ -15,12 +15,22 @@ const UserImg = styled.img`
   border-radius: 50%;
   margin-right: 4px;
 `;
+const SkeletonMove = keyframes`
+  from {
+    background-position: unset;
+  }
+  to {
+    background-position: ${PROFILE_IMAGE_SIZE * 2}px;
+  }
+  `;
 const SkeletonImg = styled.div`
   position: absolute;
   width: ${PROFILE_IMAGE_SIZE}px;
   height: ${PROFILE_IMAGE_SIZE}px;
   border-radius: 50%;
-  background-color: #dcd8d2;
+  background-image: linear-gradient(to right, #c9c9c9 8%, #7d7d7d 18%, #c9c9c9 33%);
+
+  animation: 0.8s linear 0s infinite ${SkeletonMove};
 `;
 const UserName = styled.div`
   margin-right: 4px;
